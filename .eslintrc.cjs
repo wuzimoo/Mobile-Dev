c2b542c2a27592963cec10f3098028fdb5856be4
wuzimoo <daniilbologan4@gmail.com>
2026-02-15 19:35:59 +0200

    Fix lint: use ESLint v8 + Jest globals

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
   root: true,
   extends: ['expo'],
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/*.{spec,test}.[jt]s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
